8b45df9208d66e2287a1ec9425981beab914b0f9
joezersmaniotto <joezersmaniotto5@gmail.com>
2020-09-09 09:50:35 -0300

    criado as pages de sign-in and sign-up com components separados para input e button, criado com o Unform

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { BrowserRouter } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import GlobalStyle from './styles/global';
+// import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch; /* Para se esticar*/
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  place-content: center; /*Centraliza tanto no vertical como no horizontal*/
+  align-items: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    > a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: nome;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    color: #ff9000;
+    display: block;
+    margin-top: 24px;
+    text-decoration: nome;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+    svg {
+      margin-right: 16px;
+    }
+    &:hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+  /* background-color: red;
+  width: 500px;
+  height: 900px; */
+`;
